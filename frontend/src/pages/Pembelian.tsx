5c030b89187739b5fbddf4d1c04e68e0e32e865a
zian <luthfiziannn@gmail.com>
2023-07-02 18:56:44 +0700

    input axios but fail

--- frontend/src/pages/Pembelian.tsx
+++ frontend/src/pages/Pembelian.tsx
@@ -1,54 +1,75 @@
 import { useEffect, useState } from "react"
 import { PembelianType } from "../types"
+import useRequireAuth from "../hooks/useRequireAuth"
+import axios from "axios";
+
 
 export default function Pembelian() {
-    const [dataPembelian, setDataPembelian] = useState([])
+  const [dataPembelian, setDataPembelian] = useState<PembelianType[]>([])
 
-    //fetch untuk menarik data dari database
-    const fetchPembelian = async () => {
-        const response = await fetch("http://127.0.0.1:5000/transaksi")
-        const data = await response.json()
-        setDataPembelian(data)
-    }
 
-    //semua yg ditaruh di fungsi ini akan langsung ke-run
-    useEffect(() => {
-        fetchPembelian()
+  //semua yg ditaruh di fungsi ini akan langsung ke-run
+  useEffect(() => {
+    // fetch untuk menarik data dari database
+    const dataPembelian = async () => {
+      axios.get(`http://localhost:5000/transaksi/`)
+      .then(res=>setDataPembelian(res.data))
+      .catch(error=>console.log(error));
     }
-    , [])
+    dataPembelian();
+},[]);
+
+ 
     
+    await fetch(`http://127.0.0.1:5000/transaksi/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  
+  
+  if(useRequireAuth())
     return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID Transaksi</th>
-                        <th>Nama</th>
-                        <th>Waktu Pembelian</th>
-                        <th>Total</th>
-                        <th>Ubah</th>
-                        <th>Hapus</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dataPembelian.map((pembelian: PembelianType) => (
-                        <tr key={pembelian.id_transaksi}>
-                        <td>{pembelian.id_transaksi}</td>
-                        <td>{pembelian.nama_pembeli}</td>
-                        <td>{pembelian.waktu_pembelian}</td>
-                        <td>{pembelian.total_bayar}</td>
-                        <td><button
-                        className="bg-green-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-green-800">Edit</button></td>
-                        <td><button
-                        className="bg-red-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-red-800">Hapus</button></td>
-                        </tr>
-                    ))}    
-                </tbody>
-            </table>
+      <div className="flex w-screen h-screen">
+          <table>
+            <thead>
+              <tr>
+                <th>ID Transaksi</th>
+                <th>Nama</th>
+                <th>Waktu Pembelian</th>
+                <th>Total</th>
+                <th>Ubah</th>
+                <th>Hapus</th>
+              </tr>
+            </thead>
+            <tbody>
+                {/* map untuk memunculkan data */}
+              { dataPembelian.map((pembelian: PembelianType, index: number) => (
+                <tr key={index}>
+                  <td>{pembelian.id_transaksi}</td>
+                  <td>{pembelian.nama_pembeli}</td>
+                  <td>{pembelian.waktu_pembelian}</td>
+                  <td>{pembelian.total_bayar}</td>
+                  <td>
+                    <button className="bg-blue-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-blue-800">
+                      <p>Ubah</p>
+                    </button>
+                  </td>
+                  <td>
+                    <button className="bg-red-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-red-800">
+                      <p>Hapus</p>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-            //buat tombol tambah pembelian
-            <button className="bg-blue-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-blue-800">
-                <"http://localhost:5000/tambah">Tambah Pembelian</>
-        </div>   
-        );
-    };
+          {/* Buat tombol tambah pembelian */}
+          <button className="bg-blue-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-blue-800">
+            <p>Pembelian</p>
+          </button>
+      </div>   
+    )
+  }
