e51dd0fee647e087daa3adbebaab248e2b39d8d4
zian <luthfiziannn@gmail.com>
2023-07-19 22:53:11 +0700

    create pengguna.tsx

--- frontend/src/pages/Pengguna.tsx
+++ frontend/src/pages/Pengguna.tsx
@@ -10,16 +10,16 @@ export default function Pengguna() {
 
     const fetchPengguna = async () => {
         try {
-            const response = await axios.get("http://127.0.0.1:5000")
+            const response = await axios.get("http://127.0.0.1:5000/pengguna")
 
             const fetchedData: PenggunaType[] = []
             response.data.forEach((item: any) => {
                 fetchedData.push({
-                    id_user: id_user,
-                    nama_user: nama_user,
-                    password_login: password_login,
-                    alamat: alamat,
-                    no_telp: no_telp
+                    id_user: item.id_user,
+                    nama_user: item.nama_user,
+                    password_login: item.password_login,
+                    alamat: item.alamat,
+                    no_telp: item.no_telp
                 })
 
                 setPengguna(fetchedData)
@@ -33,17 +33,17 @@ export default function Pengguna() {
 
     useEffect(() => {
         fetchPengguna()
-    }
-        , [])
+    }, [])
 
 if(useRequireAuth())
     return (
-        <MainLayout>
-            <ContentCard pageTitle="Data Pengguna">
+        // <MainLayout>
+            {/* <ContentCard pageTitle="Data Pengguna"> */}
 
                 {/* Daftar data pengguna */}
                 <div className="flex flex-row flex-wrap justify center my-[16px] gap-[28px]">
-                    { pengguna.map((item, index: string) => {
+                    { pengguna.map((item: PenggunaType, index: string) => {
+                        
                         return (
                             <div>
                                 <table>
@@ -57,7 +57,7 @@ if(useRequireAuth())
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {dataPengguna.map((pengguna: PenggunaType) => (
+                                        {pengguna.map((pengguna: PenggunaType) => (
                                           <tr key={pengguna.id_user}>
                                             <td>{pengguna.id_user}</td>
                                             <td>{pengguna.nama_user}</td>
@@ -66,16 +66,15 @@ if(useRequireAuth())
                                             <td>{pengguna.no_telp}</td>
                                             <td><button className="bg-green-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-green-800">Edit</button>
                                                 <button className="bg-red-400 text-white text-lg mt-[6px] py-[6px] rounded-full hover:cursor-pointer hover:bg-red-800">Hapus</button></td>
-                                            </tr>
-                                        )
+                                          </tr>
+                    )}
                                     </tbody>
                                 </table>
                             </div>
-                        )
-                    }
+                    )}
                 </div>
-            </ContentCard>
-        </MainLayout>
+            {/* </ContentCard> */}
+        )
+        {/* </MainLayout> */}
     )
-  )
 }
